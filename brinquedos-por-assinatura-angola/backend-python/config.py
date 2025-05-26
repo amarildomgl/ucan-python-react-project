7fcb3f11d2306495e7ade76d5085239dc3fb003e
Amarildo Miguel <amarildomgl@gmail.com>
2025-05-26 15:56:41 +0100

    add mysql project

--- brinquedos-por-assinatura-angola/backend-python/config.py
+++ brinquedos-por-assinatura-angola/backend-python/config.py
@@ -3,6 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'brinquedos.db')
+    # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'brinquedos.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/brinquedos'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '1000020998'
