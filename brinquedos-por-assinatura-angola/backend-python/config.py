8846bf65bec2f7253f8428923273f9b9125f6e70
Amarildo Miguel <amarildomgl@gmail.com>
2025-05-26 16:21:07 +0100

    add config

--- brinquedos-por-assinatura-angola/backend-python/config.py
+++ brinquedos-por-assinatura-angola/backend-python/config.py
@@ -4,6 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     # SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'brinquedos.db')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/brinquedos'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/ucan'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = '1000020998'
